f6b5428e53dc532f933bfa43adf2a7abd993564b
https-allan <code.allanjose@gmail.com>
2024-07-21 21:42:26 -0400

    Update

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/orders", (req, res) => {
-  res.render("orders");
+  res.render("orders", { isOrdersPage: true });
 });
 
 module.exports = router;
