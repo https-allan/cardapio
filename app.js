21bc6d5834a869fbf369be5499b859020ffa50a9
https-allan <code.allanjose@gmail.com>
2024-07-23 01:56:46 -0400

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const promocao = require("./routes/promotions");
 const profile = require("./routes/profile");
