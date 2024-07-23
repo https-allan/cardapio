53b6d32428e503f6483e40f82221aa5f05e14a20
https-allan <code.allanjose@gmail.com>
2024-07-23 03:23:56 -0400

    Update

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const adminMiddleware = require("../middlewares/adminMiddleware");
-
-router.use(adminMiddleware);
-
-router.get("/admin", adminMiddleware, (req, res) => {
+router.get("/admin", (req, res) => {
   res.render("panel/admin");
 });
-
-// router.get('/admin', () => {});
-// router.get('/admin', () => {});
 
 module.exports = router;
